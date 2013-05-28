f947c513b7a8d2f63943c68567f8cc6187af04d1
Tim <timbinous@gmail.com>
2013-05-27 22:29:59 -0400

    added possibility of setting the alias on cloning the dotfile repo

--- lib/dotman/git.rb
+++ lib/dotman/git.rb
@@ -1,8 +1,8 @@
 module Dotman
   class Git
-    def self.klone(git_location)
+    def self.klone(git_location, alias_name = nil)
       unless git_location.nil?
-        clone_repository(git_location)
+        clone_repository(git_location, alias_name)
       end
     end
 
@@ -10,11 +10,12 @@ module Dotman
       folder_name = git_location.scan(/[^:]+[\/]?dotfile[s?]{1}/).first.gsub('/', '_')
     end
 
-    def self.clone_repository(git_location)
+    def self.clone_repository(git_location, alias_name = nil)
       if (File.directory?("#{ENV['HOME']}/.dotman/#{folder_name(git_location)}"))
         STDOUT.puts("Dotfiles were already cloned")
       else
         system "git clone #{git_location} #{ENV['HOME']}/.dotman/#{folder_name(git_location)}"
+        Dotman::DotfileCollection.new_configuration(folder_name(git_location), alias_name)
       end
     end
   end
--- spec/integration/git_integration_spec.rb
+++ spec/integration/git_integration_spec.rb
@@ -5,18 +5,52 @@
 require 'spec_helper'
 describe Dotman::Git do
   before :all do
-    Dotman.ensure_dotman_folder_exists
-    FileUtils.rm_rf("#{ENV['HOME']}/.dotman/Timbinous_dotfiles")
+    Dotman::Base.ensure_dotman_folder_exists
   end
 
   context "cloning a new repo" do
-    before :each do
+
+    let (:git_dotfile_repo_path)    { "git@github.com:Timbinous/dotfiles.git" }
+    let (:git_folder_name)          { Dotman::Git.folder_name(git_dotfile_repo_path) }
+
+    context "when no alias passed" do
+
+      it "clones to the $HOME/.dotman folder" do
+        expect {
+        Dotman::Git.clone_repository(git_dotfile_repo_path) }.
+        to change { File.directory?("#{ENV['HOME']}/.dotman/Timbinous_dotfiles") }.to true
+      end
+
+      it "sets the yaml configuration" do
+        Dotman::DotfileCollection.find_by_alias(git_folder_name).should_not be_false
+      end
+
+      after :all do
+        FileUtils.rm_r("#{ENV['HOME']}/.dotman/dotfiles.yml")
+        FileUtils.rm_rf("#{ENV['HOME']}/.dotman/Timbinous_dotfiles")
+      end
+
     end
-    it "clones to the $HOME/.dotman folder" do
-      expect {
-      Dotman::Git.clone_repository("git@github.com:Timbinous/dotfiles.git") }.
-      to change { File.directory?("#{ENV['HOME']}/.dotman/Timbinous_dotfiles") }.to true
+
+    context "when alias was passed" do
+
+      it "clones to the $HOME/.dotman folder" do
+        expect {
+        Dotman::Git.clone_repository(git_dotfile_repo_path, 'tim') }.
+        to change { File.directory?("#{ENV['HOME']}/.dotman/Timbinous_dotfiles") }.to true
+      end
+
+      it "sets the yaml configuration" do
+        Dotman::DotfileCollection.find_by_alias('tim').should_not be_false
+      end
+
+      after :all do
+        FileUtils.rm_r("#{ENV['HOME']}/.dotman/dotfiles.yml")
+        FileUtils.rm_rf("#{ENV['HOME']}/.dotman/Timbinous_dotfiles")
+      end
+
     end
   end
+  
 
 end
