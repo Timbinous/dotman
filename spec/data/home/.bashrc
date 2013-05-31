9a7d8e7ecaa384759650b84c445207c5f69c560c
Tim <timbinous@gmail.com>
2013-05-31 16:32:19 -0400

    removing more test data

--- spec/data/home/.bashrc
+++
@@ -1,1 +0,0 @@
-/home/tim/source/dotman/spec/data/home/.dotman/Timbinous_dotfiles/.bashrc
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,11 +14,11 @@ RSpec.configure do |config|
 
   config.after(:all) do
     FileUtils.rm_rf(File.join(ENV['HOME'], '.dotman')) if File.directory? (File.join(ENV['HOME'], '.dotman'))
-    FileUtils.rm(File.join(ENV['HOME'], '.bashrc')) if File.exist? (File.join(ENV['HOME'], '.bashrc'))
-    FileUtils.rm(File.join(ENV['HOME'], '.gitignore')) if File.exist? (File.join(ENV['HOME'], '.gitignore'))
-    FileUtils.rm(File.join(ENV['HOME'], '.tmux.conf')) if File.exist? (File.join(ENV['HOME'], '.tmux.conf'))
-    FileUtils.rm(File.join(ENV['HOME'], '.vim')) if File.exist? (File.join(ENV['HOME'], '.vim'))
-    FileUtils.rm(File.join(ENV['HOME'], '.vimrc')) if File.exist? (File.join(ENV['HOME'], '.vimrc'))
-    FileUtils.rm(File.join(ENV['HOME'], '.zshrc')) if File.exist? (File.join(ENV['HOME'], '.zshrc'))
+    FileUtils.rm(File.join(ENV['HOME'], '.bashrc')) if File.symlink? (File.join(ENV['HOME'], '.bashrc'))
+    FileUtils.rm(File.join(ENV['HOME'], '.gitignore')) if File.symlink? (File.join(ENV['HOME'], '.gitignore'))
+    FileUtils.rm(File.join(ENV['HOME'], '.tmux.conf')) if File.symlink? (File.join(ENV['HOME'], '.tmux.conf'))
+    FileUtils.rm(File.join(ENV['HOME'], '.vim')) if File.symlink? (File.join(ENV['HOME'], '.vim'))
+    FileUtils.rm(File.join(ENV['HOME'], '.vimrc')) if File.symlink? (File.join(ENV['HOME'], '.vimrc'))
+    FileUtils.rm(File.join(ENV['HOME'], '.zshrc')) if File.symlink? (File.join(ENV['HOME'], '.zshrc'))
   end
 end
