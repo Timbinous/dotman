6dfbaa2813189b3cc5727cf666643b51e7de306b
Tim <timbinous@gmail.com>
2013-06-01 20:08:59 -0400

    added checks to see if files exist in lib/dotman.rb

--- lib/dotman.rb
+++ lib/dotman.rb
@@ -1,7 +1,9 @@
 require 'fileutils'
 require "dotman/version"
 require "dotman/base"
+Dotman::Base.ensure_dotman_folder_exists
+Dotman::Base.ensure_default_folder_exists
+Dotman::Base.ensure_current_dotman_file_exists
 require "dotman/git"
 require "dotman/dotfile_collection"
 require "dotman/user"
-
