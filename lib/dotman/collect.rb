d8e664228c0d4f8381c9621b704aa7cd9e99a987
Dan Bickford <danbickford007@yahoo.com>
2013-06-05 11:07:38 -0400

    specs and collect wrapped up

--- lib/dotman/collect.rb
+++ lib/dotman/collect.rb
@@ -1,15 +1,27 @@
-class Collect
+module Dotman
+  class Collect
 
-  def initialize
-    create_dotman    
-  end
-
-  def create_dotman
-    if !File.directory?("#{ENV['HOME']}/.dotman")
-      puts "creating..."
-      #FileUtils.mv("#{ENV['HOME']}/#{file}", Dotman::Base.default_folder)
+    def create_dotman
+      if !File.directory?("#{ENV['HOME']}/dotfiles")
+        FileUtils.mkdir("#{ENV['HOME']}/dotfiles")
+      end
+      collect_dotfiles
     end
 
-  end
+    def collect_dotfiles
+      puts "collecting..."
+      Dir.entries("#{ENV['HOME']}").each do |file|
+        if file =~ /^.{1}[a-zA-Z1-9]+\z/
+          copy_over_dot file
+        end
+      end
+    end
 
+    def copy_over_dot file
+      if !File.directory?("#{ENV['HOME']}/#{file}")
+        puts "copying #{file} to dotfiles"
+        FileUtils.copy "#{ENV['HOME']}/#{file}", "#{ENV['HOME']}/dotfiles"  
+      end
+    end
+  end
 end
