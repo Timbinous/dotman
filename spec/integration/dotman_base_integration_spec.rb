f9b36c66469c2f15e37781a79229bef63ae16679
Tim Nelson <timbinous@gmail.com>
2013-06-17 22:10:35 -0400

    added more specs

--- spec/integration/dotman_base_integration_spec.rb
+++ spec/integration/dotman_base_integration_spec.rb
@@ -1,20 +1,21 @@
 require 'spec_helper'
 describe Dotman::Base do
-  context "when .dotman folder doesn't exist in the home directory" do
+  describe ".ensure_dotman_folder_exists" do
     before :all do
       FileUtils.rm_rf("#{ENV['HOME']}/.dotman")
     end
 
-    it "creates a new folder" do
-      File.directory?("#{ENV['HOME']}/.dotman").should == false
-      Dotman::Base.ensure_dotman_folder_exists
-      File.directory?("#{ENV['HOME']}/.dotman").should == true
+    context "when .dotman folder doesn't exist in the home directory" do
+
+      it "creates a new folder" do
+        File.directory?("#{ENV['HOME']}/.dotman").should == false
+        Dotman::Base.ensure_dotman_folder_exists
+        File.directory?("#{ENV['HOME']}/.dotman").should == true
+      end
     end
 
     after :each do
       FileUtils.rm_rf("#{ENV['HOME']}/.dotman")
     end
-
   end
-
 end
