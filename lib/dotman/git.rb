3468d4a5d800e31e7bc3df2cd8dee03fcea29052
Tim Nelson <timbinous@gmail.com>
2013-06-20 17:09:35 -0400

    fixed after create hook

--- lib/dotman/git.rb
+++ lib/dotman/git.rb
@@ -25,7 +25,7 @@ module Dotman
       else
         system "git clone #{git_location} #{dotfile_location}"
         Dotman::DotfileCollection.new_configuration(folder_name(git_location), alias_name)
-        Dotman::AfterClone.run(File.join(Dotman::Base.dotman_folder, dotfile_location))
+        Dotman::AfterClone.run(dotfile_location)
       end
     end
   end
